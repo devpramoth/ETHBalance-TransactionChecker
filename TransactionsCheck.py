cd9a79c53f2234bbf06baeb9d97fcc2fd2d48c3d
devpramoth <41467505+devpramoth@users.noreply.github.com>
2019-01-05 19:26:28 +0100

    Update and rename TransactionsChecker.py to TransactionsCheck.py

--- TransactionsCheck.py
+++ TransactionsCheck.py
@@ -1,20 +1,18 @@
 import requests
-from pprint import pprint
 import json
 
-filename = 'addressblock3.txt'
+filename = 'abc.txt' #insert filename to read addresses from
 
 # Using the newer with construct to close the file automatically.
 with open(filename) as f:
     address = f.read().splitlines()
 
-outputtxfile = open('outputtx3start.txt', 'a')
-outputlogfile = open('outputlog3start.txt', 'a')
-
+outputtxfile = open('outputtx.txt', 'a') #writing transactions that happended
+outputlogfile = open('outputlog.txt', 'a') . #writing complete log
 trx = list()
 sum = 0
 
-#Calling the API for transactions in address
+#Calling the API from EtherScan to return transactions from an address. Insert your APIKEY
 for i in range(len(address)):
     payload = {'module': 'account', 'action': 'txlist', 'address': address[i], 'apikey': 'UDQYZ59SVS5WHH56JYTZA5WC9N6XEZW2GX'}  
     r = requests.get('https://api.etherscan.io/api', params=payload)
@@ -25,6 +23,7 @@ for i in range(len(address)):
     trx.append(transaction)
     for account in transaction['result']:
         sum = sum + int(account['value'])
+        # Print or write specifics for your own needs
         print ("Account: ", account['from'] , " to ", account['to'], "value: ", account['value'] , "Ref Block Number: ", account['blockNumber'])
         string = "Account: ", account['from'] , " to ", account['to'], "value: ", account['value'] , "Ref Block Number: ", account['blockNumber'], "##"
         outputtxfile.write(str(string))
@@ -33,7 +32,6 @@ print ("The total sum of Transactions (ETH) in all addresses is:", (sum/10000000
 string2 = "The total sum of Transactions (ETH) in all addresses is:", (sum/1000000000000000000)
 outputtxfile.write(str(string2))
 outputtxfile.close()
-
 
 outputlogfile.write(str(trx))
 outputlogfile.close()
