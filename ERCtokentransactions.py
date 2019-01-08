7e4725237bb43416d0a12820ea0eb0e4917dbbf3
devpramoth <41467505+devpramoth@users.noreply.github.com>
2019-01-08 17:43:19 +0100

    Add files via upload

---
+++ ERCtokentransactions.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+filename = 'addresses.txt'
+
+# Using the newer with construct to close the file automatically.
+with open(filename) as f:
+    address = f.read().splitlines()
+
+outputtxfile = open('outputERC20trx6.txt', 'a')
+
+sum = 0
+
+# Calling the API for transactions in address
+for i in range(len(address)):
+    payload = {'module': 'account', 'action': 'tokentx',
+               'address': address[i], 'apikey': 'UDQYZ59SVS5WHH56JYTZA5WC9N6XEZW2GX'}
+    r = requests.get('https://api.etherscan.io/api', params=payload)
+    print("This is address: ", i+1, " of ", len(address))
+    strr = "This is address: ", i+1, " of ", len(address)
+    outputtxfile.write(str(strr))
+    transaction = r.json()
+    for account in transaction['result']:
+        sum = sum + int(account['value'])
+        print("Account: ", account['from'], " to ", account['to'], "value: ",
+              account['value'], "Ref Block Number: ", account['blockNumber'])
+        string = "Account: ", account['from'], " to ", account['to'], "value: ", account[
+            'value'], "Ref Block Number: ", account['blockNumber'], "##"
+        outputtxfile.write(str(string))
+
+print("The total sum of Transactions (ETH) in all addresses is:",
+      (sum/1000000000000000000))
+string2 = "The total sum of Transactions (ETH) in all addresses is:", (
+    sum/1000000000000000000)
+outputtxfile.write(str(string2))
+outputtxfile.close()
+
+
+# old APIKEY: UDQYZ59SVS5WHH56JYTZA5WC9N6XEZW2GX AJZ71AEG686X2KR34DQHSJVJSN2K6E8S1A
